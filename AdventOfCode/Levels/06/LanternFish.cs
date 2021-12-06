35efecba705506ac9de74ef6df7e26bf7dc417da
Milan Augustin <milan.augustin.bb@gmail.com>
2021-12-06 16:00:23 +0100

    Day 6: LanternFish - part 2

--- AdventOfCode/Levels/06/LanternFish.cs
+++ AdventOfCode/Levels/06/LanternFish.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace AdventOfCode.Levels._06;
 
 public class LanternFish : ALevel<long>
@@ -10,17 +12,45 @@ public class LanternFish : ALevel<long>
     {
         var initialFishes = ReadLine(reader)
             .SelectMany(line => line.Split(','))
-            .Select(s => new Fish(int.Parse(s)))
+            .Select(int.Parse)
             .ToList();
 
-        var limit = Setup.Round == 1 ? 80 : 256;
+        var timing = new List<TimeSpan>(256);
+
+        var fishes = new LinkedList<int>(initialFishes);
+        var daysLimit = Setup.Round == 1 ? 80 : 256;
         var daysPassed = 0;
-        var query = initialFishes.AsEnumerable();
-        while (daysPassed++ <= limit)
+        while (daysPassed++ < daysLimit)
         {
-            query = query.SpawnFish().Select(fish => --fish);
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var counter = 0;
+            var limit = fishes.Count;
+            var fish = fishes.First;
+            while (counter++ < limit && fish is not null)
+            {
+                if (fish.ValueRef == 0)
+                {
+                    fishes.AddLast(8);
+                    fish.ValueRef = 6;
+                }
+                else
+                {
+                    fish.ValueRef--;
+                }
+                fish = fish.Next;
+            }
+
+            timer.Stop();
+            var time = timer.Elapsed;
+            timing.Add(time);
+
+            var avgTime = new TimeSpan((long) timing.Average(t => t.Ticks));
+            Console.WriteLine($"Day: {daysPassed}");
+            Console.WriteLine($"T1: {time:g} \tA: {avgTime:g} \tCount: {fishes.Count,12}");
         }
 
-        return query.LongCount();
+        return fishes.Count;
     }
 }
